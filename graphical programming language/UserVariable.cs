73575326993f2c984f9e9e884cc9a2def3cf6467
maskedyeti <148262638+maskedyeti@users.noreply.github.com>
2023-12-22 17:09:42 +0000

    UserVariableFactoryClass created along with IVariableFactory interface

---
+++ graphical programming language/UserVariable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphical_programming_language
+{
+    public interface IVariable
+    {
+        string Name { get; }
+        int Value { get; set; }
+
+    }
+    public class UserVariable : IVariable
+    {
+        public string Name { get; private set; }
+        public int Value { get; set; }
+
+        public UserVariable(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+
+    }
+}
---
+++ graphical programming language/UserVariableFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphical_programming_language
+{
+    public interface IVariableFactory
+    {
+        IVariable CreateVariable(string name, int value);
+    }
+    public class UserVariableFactory : IVariableFactory
+    {
+        public IVariable CreateVariable(string name, int value)
+        {
+            return new UserVariable(name, value);
+        }
+    }
+}
